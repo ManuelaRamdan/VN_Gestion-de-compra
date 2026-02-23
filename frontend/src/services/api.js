3d399f8e4cbdd3320d0e5d0bf9ff3c6b0f79ac90
Manuela <mramdan@gmail.com>
2026-02-23 11:59:56 -0300

    resolviendo problemas

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://192.168.1.200:8081"
-
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081"
 });
 
 /*
